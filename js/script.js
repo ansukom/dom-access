a90691ac2f317b5871c1ac2f776db5bc5764ab7d
ansumanakomar <ansukoma@gmail.com>
2015-09-23 21:29:12 -0500

    almost done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+var menuitem = document.getElementsByClassName("menu-item");
+console.log(menuitem);
+for (var i = 0; i < menuitem.length; i++) {
+    current = menuitem[i];
+    var asideimage = document.getElementById("aside-image");
+    asideimage.removeAttribute("class");
+    var image = current.nextElementSibling;
 
+    image.setAttribute("class", "hide");
+    current.onclick = function () {
+        var image = this.nextElementSibling;
+        var src = image.getAttribute("src");
+        asideimage.setAttribute("src", src);
+        //onclick = "myFunction()" > onclick
+    }
+}
